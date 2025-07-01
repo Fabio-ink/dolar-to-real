3a3a636b60d40708e3fcefa3fb5039723a8e8a56
Fabio <fabiopradaa@gmail.com>
2025-07-01 18:50:44 -0300

    Comentários na classe CuurencyConverter

--- util/CurrencyConverter.java
+++ util/CurrencyConverter.java
@@ -2,6 +2,8 @@ package util;
 
 public class CurrencyConverter {
 
+    // Valor do IOF de 6%.
+
     public static Double converter(double dolar, double quantity) {
         return dolar * quantity + (dolar * quantity * 0.06);
     }
