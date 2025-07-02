db54063ddd1da896e8f69feb88a51338ba738626
Fabio <fabiopradaa@gmail.com>
2025-07-02 19:48:57 -0300

    Ajuste

--- application/Program.java
+++ application/Program.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 import util.CurrencyConverter;
 
-import java.util.Scanner;
-
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
